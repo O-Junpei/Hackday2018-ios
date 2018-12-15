04c3cfed020fd34d97e4e77f4312a61aaa2e72d1
junpei ono <snorlax.chemist.and.jazz@gmail.com>
2018-12-15 20:54:48 +0900

    init commit

---
+++ Hackday2018-ios/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Hackday2018-ios
+//
+//  Created by junpei ono on 2018/12/15.
+//  Copyright © 2018 junpei ono. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
